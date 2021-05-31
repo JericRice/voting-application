f16ea45154a1bb6fac986e9bf72363a4bee6b13f
Joe <jeric720@icloud.com>
2021-05-31 12:08:44 -0400

    duplicate votes are now blocked, added voted.html page

--- src/main/java/com/jerictech/votingsystem/controller/VotingController.java
+++ src/main/java/com/jerictech/votingsystem/controller/VotingController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 
@@ -31,8 +32,10 @@ public class VotingController {
     }
 
     @RequestMapping("/doLogin")
-    public String doLogin(@RequestParam String name,Model model) {
+    public String doLogin(@RequestParam String name, Model model, HttpSession session) {
         Citizen citizen = citizenRepo.findByName(name);
+        session.setAttribute("citizen", citizen);
+
         if(!citizen.getHasVoted()) {
             List<Candidate> candidates = candidateRepo.findAll();
             model.addAttribute("candidates",candidates);
@@ -43,14 +46,32 @@ public class VotingController {
     }
 
     @RequestMapping("/voteFor")
-    public String voteFor(@RequestParam Long id) {
-        Candidate c = candidateRepo.findById((long)id);
-        c.setNumberOfVotes(c.getNumberOfVotes()+1);
-        candidateRepo.save(c);
+    public String voteFor(@RequestParam Long id, HttpSession session) {
+        Citizen citizen = (Citizen) session.getAttribute("citizen");
 
-        return "voted";
+        /*
+            If voter hasn't voted yet, save vote and increment total vote by one
+         */
+        if (!citizen.getHasVoted()) {
+            citizen.setHasVoted(true);
+
+            Candidate c = candidateRepo.findById((long) id);
+            c.setNumberOfVotes(c.getNumberOfVotes() + 1);
+
+            candidateRepo.save(c);
+            citizenRepo.save(citizen);
+
+            return "voted"; // return successfully voted confimation page
+        }
+        /*
+            If voter has voted, serve up page that states that they have already voted
+         */
+        return "alreadyVoted";
+    }
+
+
 
     }
 
 
-}
+
